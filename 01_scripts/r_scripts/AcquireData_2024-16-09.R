7ad6b546209f2b129db8ba459e834fc4923ff284
pklund56 <73411207+pklund56@users.noreply.github.com>
2024-09-16 21:12:57 -0700

    Rmarkdown manuscript draft.

---
+++ 01_scripts/r_scripts/AcquireData_2024-16-09.R
@@ -0,0 +1,11 @@
+# Wrote a script to acquire data and then save it as a .csv
+# To save time, I am just using the iris dataset built-in to R. 
+# The data I was originally going to use was my anemone microbe data, but it's sequencing data that doesn't usually get saved as a .csv
+
+
+# Acquire data
+data = iris
+
+
+# Save as .csv in rawdata folder
+write.csv(data, "00_rawdata/IrisData_2024-16-09.csv")
